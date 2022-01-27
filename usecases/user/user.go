5356463a38b250f2620541c993f546ab28faaf7c
wagnerribas <wagnerribas63@gmail.com>
2022-01-27 19:21:38 -0300

    [ USECASES:USER ] Update

--- usecases/user/user.go
+++ usecases/user/user.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"errors"
 	"go-clean-arch/entities"
 	"go-clean-arch/repositories"
 )
@@ -11,9 +12,18 @@ type ICreateUserUseCaseDTO struct {
 	Password string `json:"password"`
 }
 
+type IUpdateUserUseCaseDTO struct {
+	ID       string `json:"id,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+	IsAdmin  bool   `json:"is_admin,omitempty"`
+}
+
 type IUsersUseCases interface {
 	Create(data ICreateUserUseCaseDTO) error
 	FindAll() ([]entities.User, error)
+	Update(data IUpdateUserUseCaseDTO) error
 }
 
 type userUserCases struct {
@@ -37,4 +47,24 @@ func (usecase *userUserCases) Create(data ICreateUserUseCaseDTO) error {
 
 func (usecase *userUserCases) FindAll() ([]entities.User, error) {
 	return usecase.repositories.User.FindAll()
+}
+
+func (usecase *userUserCases) Update(data IUpdateUserUseCaseDTO) error {
+	_, err := usecase.repositories.User.FindById(data.ID)
+	if err != nil {
+		return errors.New("User not found.")
+	}
+
+	err = usecase.repositories.User.Update(entities.User{
+		Name:     data.Name,
+		Email:    data.Email,
+		Password: data.Password,
+		IsAdmin:  data.IsAdmin,
+	})
+
+	if err != nil {
+		return errors.New("Error trying to update user")
+	}
+
+	return nil
 }
